b2222d5674dbf50e2d617eeda6d7de6756b6fde9
100522202 <100522202@alumnos.uc3m.es>
2025-11-18 14:17:18 +0100

    Parte-1 falta escribir en salida correctamente y que se imprima por terminal correctamente

---
+++ imprimir_solucion.py
@@ -0,0 +1,49 @@
+def imprimir_solucion_formato(entrada:dict, n):
+    """Método que va a imprimir en el formato que pide
+    el enunciado """
+    texto = ""
+
+    #Linea superior
+    texto = "+---" * n + "+\n"
+
+    for fila in range(n):
+        texto += "| "
+        for col in range(n):
+            valor = entrada[f"C{fila}{col}"]
+            if valor == 1:
+                texto += "O "
+            else:
+                texto += "X "
+            texto += "| "
+        texto += "\n"
+    texto += "+---" * n + "+\n"
+
+    print(texto)
+
+def imprimir_entrada_formato(lineas):
+    """Imprime el fichero de entrada en formato tablero"""
+
+    n = len(lineas)
+
+    texto = "+---" * n + "+\n"
+
+    for fila in range(n):
+        texto += "| "
+        for col in range(n):
+            c = lineas[fila][col]
+            if c == '.':
+                texto += "  | "
+            elif c == 'X':
+                texto += "X | "
+            elif c == 'O':
+                texto += "O | "
+            else:
+                raise ValueError(f"Carácter inválido: {c}")
+        texto += "\n"
+            
+    texto += "+---" * n + "+\n"
+
+    print(texto)
+
+
+
---
+++ lector_entrada.py
@@ -0,0 +1,45 @@
+
+def leer_fichero(fichero_entrada):
+
+    lineas = []
+    with open(fichero_entrada) as f: 
+        for linea in f:
+            linea_limpia = linea.strip()
+            if linea_limpia != "": #si la linea no está vacia
+                lineas.append(linea_limpia)
+
+    #Número de filas columnas
+    n = len(lineas)
+
+    #Validamos que sea cuadrado
+    for linea in lineas:
+        if len(linea) != n:
+            raise ValueError("El tablero dado no es cuadrado")
+    
+    return n, lineas
+
+
+def procesar_casillas(n, lineas:list):
+    """Método que va a procesar cada casilla"""
+    lista_fijas = []
+    for i in range(n):
+        for j in range(n):
+            caracter = lineas[i][j]
+
+            casilla = f"C{i}{j}"
+
+            if caracter == 'X':
+                lista_fijas.append((casilla, 0))
+            elif caracter == 'O':
+                lista_fijas.append((casilla, 1))
+            elif caracter == '.':
+                continue
+            else:
+                raise ValueError(f"Carácter inválido '{caracter}' en ({i},{j})")
+
+    return lista_fijas
+
+def main(ruta_fichero):
+    n, lineas = leer_fichero(ruta_fichero)
+    lista_fijas = procesar_casillas(n, lineas)
+    return n, lista_fijas, lineas
